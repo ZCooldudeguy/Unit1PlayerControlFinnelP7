0c0f1bf7bb3d96d2154419fa286b0f9e931055b4
ZCooldudeguy <731256@student.dusd.net>
2026-02-03 15:26:38 -0800

    did stuff
    finished camera and stated third part

---
+++ P7PlayerControlFinnell/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+}
